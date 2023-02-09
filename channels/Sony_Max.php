c84ef0496bc9557cef4fef8f26c7c77bc656f69b
LSMEENA <82662949+ls90901@users.noreply.github.com>
2023-02-09 22:21:49 +0530

    L_S_MEENA

---
+++ channels/Sony_Max.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Plyr - A simple, customizable HTML5 Video, Audio, YouTube and Vimeo player</title>
+<meta name="description" property="og:description" content="A simple HTML5 media player with custom controls and WebVTT captions." />
+<meta name="author" content="Sam Potts" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<link rel="icon" href="https://cdn.plyr.io/static/icons/favicon.ico" />
+<link rel="icon" type="image/png" href="https://cdn.plyr.io/static/icons/32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="https://cdn.plyr.io/static/icons/16x16.png" sizes="16x16" />
+<link rel="apple-touch-icon" sizes="180x180" href="https://cdn.plyr.io/static/icons/180x180.png" />
+
+<meta property="og:title" content="Plyr - A simple, customizable HTML5 Video, Audio, YouTube and Vimeo player" />
+<meta property="og:site_name" content="Plyr" />
+<meta property="og:url" content="https://plyr.io" />
+<meta property="og:image" content="https://cdn.plyr.io/static/icons/1200x630.png" />
+
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@sam_potts" />
+<meta name="twitter:creator" content="@sam_potts" />
+<meta name="twitter:card" content="summary_large_image" />
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/plyr@3.6.2/dist/plyr.css" />
+
+<link rel="preload" as="font" crossorigin type="font/woff2" href="https://cdn.plyr.io/static/fonts/gordita-medium.woff2" />
+<link rel="preload" as="font" crossorigin type="font/woff2" href="https://cdn.plyr.io/static/fonts/gordita-bold.woff2" />
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-132699580-1"></script>
+<script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'UA-132699580-1');
+    </script>
+
+    
+</head>
+<body>
+
+<video id="player" controls preload="metadata" poster="https://cdn2.mynrwsf.xyz/red.php?id=3" class="plyr">
+
+<source src="https://pubads.g.doubleclick.net/ssai/event/oJ-TGgVFSgSMBUoTkauvFQ/master.m3u8" type="application/x-mpegURL"/>
+
+</video>
+
+</div>
+<ul>
+<li class="plyr__cite plyr__cite--video" hidden>
+<small>
+<svg class="icon">
+<title>HTML5</title>
+<path d="M14.738.326C14.548.118 14.28 0 14 0H2c-.28 0-.55.118-.738.326S.98.81 1.004 1.09l1 11c.03.317.208.603.48.767l5 3c.16.095.338.143.516.143s.356-.048.515-.143l5-3c.273-.164.452-.45.48-.767l1-11c.026-.28-.067-.557-.257-.764zM12 4H6v2h6v5.72l-4 1.334-4-1.333V9h2v1.28l2 .666 2-.667V8H4V2h8v2z"></path>
+</svg>
+
+</li>
+<li class="plyr__cite plyr__cite--audio" hidden>
+<small>
+<svg class="icon" title="HTML5">
+<title>HTML5</title>
+<path d="M14.738.326C14.548.118 14.28 0 14 0H2c-.28 0-.55.118-.738.326S.98.81 1.004 1.09l1 11c.03.317.208.603.48.767l5 3c.16.095.338.143.516.143s.356-.048.515-.143l5-3c.273-.164.452-.45.48-.767l1-11c.026-.28-.067-.557-.257-.764zM12 4H6v2h6v5.72l-4 1.334-4-1.333V9h2v1.28l2 .666 2-.667V8H4V2h8v2z"></path>
+</svg>
+
+</div>
+<aside>
+<svg class="icon">
+<title>Twitter</title>
+<path d="M16,3c-0.6,0.3-1.2,0.4-1.9,0.5c0.7-0.4,1.2-1,1.4-1.8c-0.6,0.4-1.3,0.6-2.1,0.8c-0.6-0.6-1.5-1-2.4-1
+       C9.3,1.5,7.8,3,7.8,4.8c0,0.3,0,0.5,0.1,0.7C5.2,5.4,2.7,4.1,1.1,2.1c-0.3,0.5-0.4,1-0.4,1.7c0,1.1,0.6,2.1,1.5,2.7
+       c-0.5,0-1-0.2-1.5-0.4c0,0,0,0,0,0c0,1.6,1.1,2.9,2.6,3.2C3,9.4,2.7,9.4,2.4,9.4c-0.2,0-0.4,0-0.6-0.1c0.4,1.3,1.6,2.3,3.1,2.3
+       c-1.1,0.9-2.5,1.4-4.1,1.4c-0.3,0-0.5,0-0.8,0c1.5,0.9,3.2,1.5,5,1.5c6,0,9.3-5,9.3-9.3c0-0.1,0-0.3,0-0.4C15,4.3,15.6,3.7,16,3z"></path>
+</svg>
+<p>
+If you think Plyr's good,
+<a href="https://" target="_blank" class="js-shr">tweet it</a
+        >
+👍
+</p>
+</aside>
+<script src="https://cdn.plyr.io/3.7.2/demo.js" crossorigin="anonymous"></script>
+</body>
+</html>
